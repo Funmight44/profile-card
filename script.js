43c6a5872500d33d1bc8bb6b9a53d875a7839a90
Funmilayo Akano <akanofunmilayo44@gmail.com>
2025-01-29 21:43:59 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const date = document.querySelector('.date');
+ 
+
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    date.textContent = timeString
+}
+
+// Update time every second
+setInterval(updateTime, 1000);
+
+// Initialize clock immediately
+updateTime();
+
+
+// now.toUTCString();  
+
+// console.log(now)
+
+// date.textContent = now;
